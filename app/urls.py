6de89f2eccaefa881f00e3d2f96389573ef7a3cb
dike.zhang <dike.zhang@gmail.com>
2016-12-23 08:35:10 -0500

    url

--- app/urls.py
+++ app/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 import views
 
 from rest_framework import routers
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView, LogoutView
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -27,6 +27,8 @@ router.register(r'accounts', AccountViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
 ]
 
 # settings for development environment DEBUG
